b67477a7f536e3a18f14dc9b05ba516e4a497129
Kalyani Meshram <kkmeshram16@gmail.com>
2026-09-16 01:44:24 +0530

    updated  Union of Two Arrays

--- Arrays/unionArray.py
+++ Arrays/unionArray.py
@@ -9,31 +9,30 @@ class Solution:
 
             if nums1[i] < nums2[j]:
 
-                if not union or union[-1] != nums1[i]:
+                if len(union) == 0 or union[-1] != nums1[i]:
                     union.append(nums1[i])
                 i += 1
 
             # nums2[j] is smaller OR both are equal
             else:
-
-                if not union or union[-1] != nums2[j]:
+                if len(union) == 0 or union[-1] != nums2[j]:
                     union.append(nums2[j])
                 j += 1
 
         # Remaining elements of nums1
         while i < len(nums1):
-            if not union or union[-1] != nums1[i]:
+            if len(union) == 0 or union[-1] != nums1[i]:
                 union.append(nums1[i])
             i += 1
 
         # Remaining elements of nums2
         while j < len(nums2):
-            if not union or union[-1] != nums2[j]:
+            if len(union) == 0 or union[-1] != nums2[j]:
                 union.append(nums2[j])
             j += 1
 
         return union
 obj = Solution()
-nums1 = [1, 2, 4, 5, 6]
-nums2 = [2, 3, 5, 7]
+nums1 = [1, 1, 2, 3, 4, 5]
+nums2 = [2, 3, 4, 4, 5, 6]
 print(obj.unionArray(nums1, nums2))
